5c36dfeb3d5a1726b28cdb47be8483fcf8d1e11b
chandu779979 <bobbalachandu25@gmail.com>
2026-07-31 17:33:32 +0530

    Add files via upload

---
+++ day3/s.java
@@ -0,0 +1,17 @@
+package day3;
+
+import java.util.Scanner;
+
+public class s {
+    public static void main(String args[]) {
+        Scanner sc= new Scanner(System.in);
+        int i = 0;
+        switch(i) {
+            case 0:i++;
+            case 1:i++;
+            case 2:++i;
+            System.out.println(i++);
+        }
+    }
+}
+    
